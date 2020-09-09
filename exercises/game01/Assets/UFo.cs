8c0dc81cd6243d7119bd3b738ba901b0cf0b5e17
JKreates <70207521+JKreates@users.noreply.github.com>
2020-09-08 20:50:21 -0400

    game01 Exercise

---
+++ exercises/game01/Assets/UFo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFo : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 100);
+    }
+}
---
+++ exercises/game01/Assets/spin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left * Time.deltaTime * 100);
+        transform.Rotate(Vector3.up * Time.deltaTime * 100);
+    }
+}
